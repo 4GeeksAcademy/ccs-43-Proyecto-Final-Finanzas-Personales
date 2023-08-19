4a687e1417ec6c80388781a2e4d4a5e96c785361
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-08-19 18:45:40 +0000

    v1.2 del navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,12 +4,13 @@ import Brand from "../../img/Wallet-Rack_Logo_sinFondo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar d-flex justify-content-around navbar-light p-2">
+		<nav className="navbar d-flex justify-content-around navbar-light p-0">
 			<div className="wrap d-flex align-items-center justify-content-between" style={{width: "90%"}}>
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1"><img src={Brand} className="logo-img img-fluid"/></span>
 				</Link>
 				<div className="d-flex justify-content-between">
+					<a href="nosotros" className="text-light text-decoration-none me-4">Nosotros</a>
 					<Link to="/demo">
 						<button className="btn btn-outline-light me-3">Iniciar sesión</button>
 					</Link>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -17,8 +17,7 @@ export const Home = () => {
 			</div>
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
+				<a href="https://start.4geeksacademy.com/starters/react-flask">Read documentation
 				</a>
 			</p>
 		</div>
