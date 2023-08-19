a9760d60b5b70d9d64bcb2bf2998a7d420a8ad6b
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-08-19 16:49:08 +0000

    1era version del navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Brand from "../../img/Wallet-Rack_Logo_sinFondo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar d-flex justify-content-around navbar-light p-2">
+			<div className="wrap d-flex align-items-center justify-content-between" style={{width: "90%"}}>
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1"><img src={Brand} className="logo-img img-fluid"/></span>
 				</Link>
-				<div className="ml-auto">
+				<div className="d-flex justify-content-between">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-outline-light me-3">Iniciar sesión</button>
 					</Link>
+					<button className="btn btn-outline-light">Registrarse</button>
 				</div>
 			</div>
 		</nav>
