d4184e05de764f70defcb88d7d8d3b0d799f5bf7
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-08-22 12:03:21 +0000

    se agreraron las 5 etiquetas de promocion en los slides con la imagen modelo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -11,6 +11,8 @@ export const Home = () => {
 				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
 				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
 				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
 			</div>
 			<div className="carousel-inner">
 				<div className="carousel-item active">
@@ -21,17 +23,31 @@ export const Home = () => {
 				</div>
 				</div>
 				<div className="carousel-item">
-				<img src="..." className="d-block w-100" alt="..."/>
+				<img src="https://res.cloudinary.com/dronv3ars/image/upload/v1692704271/Imagen1_r6j4ee.jpg" className="d-block mx-auto img-fluid" alt="..."/>
 				<div className="carousel-caption d-none d-md-block">
-					<h5>Second slide label</h5>
-					<p>Some representative placeholder content for the second slide.</p>
+					<h5>Visualiza tu progreso de un vistazo</h5>
+					<p>Observa tu balance financiero en gráficos visuales y atractivos. Nuestros gráficos interactivos te brindan una perspectiva clara de cómo evolucionan tus finanzas a lo largo del tiempo.</p>
 				</div>
 				</div>
 				<div className="carousel-item">
-				<img src="..." className="d-block w-100" alt="..."/>
+				<img src="https://res.cloudinary.com/dronv3ars/image/upload/v1692704271/Imagen1_r6j4ee.jpg" className="d-block mx-auto img-fluid" alt="..."/>
 				<div className="carousel-caption d-none d-md-block">
-					<h5>Third slide label</h5>
-					<p>Some representative placeholder content for the third slide.</p>
+					<h5>A tu manera, tus categorías</h5>
+					<p>Personaliza tus categorías de ingresos y egresos para que se ajusten a tu estilo de vida único. Adapta la plataforma a tus necesidades y prioridades financieras.</p>
+				</div>
+				</div>
+				<div className="carousel-item">
+				<img src="https://res.cloudinary.com/dronv3ars/image/upload/v1692704271/Imagen1_r6j4ee.jpg" className="d-block mx-auto img-fluid" alt="..."/>
+				<div className="carousel-caption d-none d-md-block">
+					<h5>Metas de ahorro alcanzables</h5>
+					<p>Establece objetivos de ahorro personalizados y alcanzables. Nuestra plataforma te ayuda a trazar un camino claro hacia tus sueños financieros, ya sea un viaje, un nuevo hogar o cualquier otro objetivo.</p>
+				</div>
+				</div>
+				<div className="carousel-item">
+				<img src="https://res.cloudinary.com/dronv3ars/image/upload/v1692704271/Imagen1_r6j4ee.jpg" className="d-block mx-auto img-fluid" alt="..."/>
+				<div className="carousel-caption d-none d-md-block">
+					<h5>Conversión instantánea y precisa</h5>
+					<p>¿Operas en distintas monedas? No hay problema. Convierte al instante tus registros de Bolívares a Dólares. Mantén un seguimiento preciso sin complicaciones.</p>
 				</div>
 				</div>
 			</div>
