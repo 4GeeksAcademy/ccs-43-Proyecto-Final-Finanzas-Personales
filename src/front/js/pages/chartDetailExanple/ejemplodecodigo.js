bdc1de1790e12356748e9642156387af9922debe
Screin208 <49405366+Screin208@users.noreply.github.com>
2023-08-27 10:47:39 +0000

    graficas falta los imput estan dandome error

--- src/front/js/pages/chartDetailExanple/ejemplodecodigo.js
+++ src/front/js/pages/chartDetailExanple/ejemplodecodigo.js
@@ -1,11 +1,23 @@
 import React from "react";
 import axios from "axios";
-// import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Bar, Line, Pie } from 'react-chartjs-2';
 import {Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler} from 'chart.js';
 ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler,ArcElement);
     
     export const CharDetail = () => {
+    const [fechaInicio, setFechaInicio] = useState('');
+    const [fechaFin, setFechaFin] = useState('');
+
+    const handleFechaInicioChange = (event) => {
+        setFechaInicio(event.target.value);
+      };
+    
+      const handleFechaFinChange = (event) => {
+        setFechaFin(event.target.value);
+      };
+
+
   
         // BarsChart
         const beneficios = [72, 56, 20, 36, 80, 40, 30, -20, 25, 30, 12, 60];
@@ -50,12 +62,32 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, To
                 },
             ],
         };
-
-       
        
     return (
     <div class="container row justify-content-center col-md-8 text-center"  >
-          <div>
+        <h1>Selecciona dos fechas</h1>
+        <div>
+        <label>Fecha de Inicio:</label>
+        <input
+            type="date"
+            value={fechaInicio}
+            onChange={handleFechaInicioChange}
+        />
+        </div>
+        <div>
+        <label>Fecha de Fin:</label>
+        <input
+            type="date"
+            value={fechaFin}
+            onChange={handleFechaFinChange}
+        />
+        </div>
+        <div>
+        <p>Fecha de Inicio seleccionada: {fechaInicio}</p>
+        <p>Fecha de Fin seleccionada: {fechaFin}</p>
+        </div>
+
+        <div>
             <Bar data={midata} options={misoptions} />
         </div>
         <div>
