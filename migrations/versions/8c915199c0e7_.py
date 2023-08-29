926bb13acd4f8fa7f0777af457d4ed05a35f8fa9
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-08-29 00:04:44 +0000

    pull

--- migrations/versions/8c915199c0e7_.py
+++ migrations/versions/8c915199c0e7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ef777603415b
+Revision ID: 8c915199c0e7
 Revises: 
-Create Date: 2023-08-28 22:17:31.445693
+Create Date: 2023-08-29 00:03:19.771184
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ef777603415b'
+revision = '8c915199c0e7'
 down_revision = None
 branch_labels = None
 depends_on = None
