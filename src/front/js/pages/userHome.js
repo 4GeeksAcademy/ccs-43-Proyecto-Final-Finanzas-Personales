7e0adedfa7f348e20f94f2a0f7e88af8aa2ac601
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-08-31 02:35:47 +0000

    se modifico el simbolo Bs en el renderizado de userHome

--- src/front/js/pages/userHome.js
+++ src/front/js/pages/userHome.js
@@ -182,8 +182,8 @@ export const UserHome = () => {
                 <div className="mininavbarUserHome">
                     <p className="h6NicoUserHomejs"><strong>Valor del $ hoy:</strong></p><i className="fa-solid fa-dollar-sign" style={{color: "white"}}></i>
                 </div>
-                <h1 id="sumatotaldemovimientosnegro" className="sumatotaldemovimientos">BCV: $ {store.dolarBcv}</h1>
-                <h1 id="sumatotaldemovimientosnegro" className="sumatotaldemovimientos">Paralelo: $ {store.dolarParalelo}</h1>
+                <h1 id="sumatotaldemovimientosnegro" className="sumatotaldemovimientos">BCV: Bs {store.dolarBcv}</h1>
+                <h1 id="sumatotaldemovimientosnegro" className="sumatotaldemovimientos">Paralelo: Bs {store.dolarParalelo}</h1>
             </div>
             <div className="container containerDechartHomejs">
                 {/* <h3 className="pdegrafica1Nico">Observa diariamente como se mueven tus estadísticas del mes actual!</h3> */}
