353436910b646e0ce2c996f13d106f790fbf1711
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-09-07 00:04:15 +0000

    ajuste en margen del footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-3 py-3 text-center">
 		<p>
 			<i className="fa-regular fa-copyright" /> Wallet-Rack. 2023.
 		</p>
